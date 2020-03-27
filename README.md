289e0843e2b0326b2dc26e878433bf59f0926736
mschemmel <mschemmel@online.de>
2020-03-27 21:36:33 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# Motif
+
+A small python script to locate motifs in nucleotide sequences. The underlying idea was to identify binding sites that comprise only a few nucleotides in target sequences, such as those required for miRNA or primer binding sites. Of course, it can also be used as a general tool to identify partial regions in nucleotide sequences. The only requirement is a 100 % match of the sequences, the identification of degenerated sequences is not yet supported.
+
+## Getting Started
+It is written in python3.
+
+## Prerequisites
+It uses the package ['tqdm'](https://github.com/tqdm/tqdm) to track progress. If it is not already installed, you can easily do this with:
+```{bash}
+pip3 install tqdm 
+```
+
+## Usage
+```{python}
+python3 motifs.py -t template.fa -q query.fa -o /your/output/path -p myproject
+```
+### Arguments:
+
+    -t path to template file
+    -q path to query file
+    -o path to output folder
+    -p name of your project
+
+If the output path (-o) is not specified, the current working directory is used. All other arguments are required.
+## Test
+
+The 'data' folder contains two files (template.fa; query.fa) with arbitrarily generated nucleotide sequences for testing purposes. Try them out using:
+
+```{python}
+python3 motifs.py -t ./data/template.fa -q ./data/query.fa -p testrun
+```
+## Output
+The output is structured into two files:
+
+ * mapping.txt
+    * contains a simple text based visualization of template sequences with at least one hit
+ * summary.txt:
+    * contains a tabular conclusion of all template sequences with at least on hit and their location
+
---
+++ motifs.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python3
+import os
+import sys
+import argparse
+import datetime
+from tqdm import tqdm
+
+def import_sequences(filepath):
+    sequences = {}
+    header = ""
+
+    # open sequence file and populate dictionary
+    with open(filepath, "r") as fle:
+        for line in fle:
+            if not line.strip():
+                continue
+            if line.startswith(">"):
+                header = line.replace(">", "").strip()
+                sequences[header] = ""
+            else:
+                sequences[header] += line.strip().upper()
+    return sequences
+
+def findall(base, pattern):
+    binding_sites_per_target = {}
+    # loop through all targets
+    for id_target, seq_target in tqdm(base.items()):
+        binding_sites = {}
+        for id_query, seq_query in pattern.items():
+            matches = []
+
+            # find all binding sites and append to 'matches'
+            hit = seq_target.find(seq_query)
+            while hit != -1:
+                matches.append(hit)
+                hit = seq_target.find(seq_query, hit + 1)
+
+            if matches:
+                binding_sites[seq_query] = matches
+                binding_sites_per_target[id_target] = binding_sites
+
+    return binding_sites_per_target
+
+def illustrate(template, summary):
+    for target, binding_sites in summary.items():
+        yield ">{}:{}\n{}".format(target, str(len(template[target])), template[target])
+        for query_seq, sites in binding_sites.items():
+            for site in sites:
+                yield " " * site + query_seq + ":" + "(" + str(len(query_seq)) + ")" + ":" + str(site) + ":" + str(site + len(query_seq))
+
+def now():
+    return str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+
+def save_mapping(templ, align, filepath):
+    with open(filepath, "a") as fle:
+        fle.write("\n".join(illustrate(templ, align)))
+
+def save_report(template, align, filepath):
+    with open(filepath, "a") as fle:
+        fle.write("{}\t{}\t{}\t{}\t{}\t{}\n".format("target", "target_length", "query", "query_length", "start", "stop"))
+        for k, v in align.items():
+            for a, b in v.items():
+                for item in b:
+                    fle.write("\t".join(map(str, [k, len(template[k]), a, len(a), int(item), int(item) + len(a)])) + "\n")
+
+def main():
+    # handle command line options
+    parser = argparse.ArgumentParser(description="Program to find all occurences of queries in template sequences.")
+    parser.add_argument("-t", "--targets", help="Path to your target fasta file")
+    parser.add_argument("-q", "--queries", help="Path to your query fasta file")
+    parser.add_argument("-o", "--output", help="Path to your output directory")
+    parser.add_argument("-p", "--project", help="Project ID")
+    args = parser.parse_args()
+
+    # check if all necessary filepaths are provided
+    # check targets and query
+    if args.targets:
+        if args.queries:
+            pass
+        else:
+            print("Please provide a valid path for your query file.")
+            sys.exit(0)
+    else:
+        print("Please provide a valid path for your target file.")
+        sys.exit(0)
+
+    # check output
+    if args.output:
+        out_dir = args.output
+    else:
+        out_dir = os.getcwd()
+
+    # check project
+    if args.project:
+        out_dir = os.path.join(out_dir, args.project)
+        if os.path.isdir(out_dir):
+            answer = input("WARNING: Project folder already exists. Overwrite [y/n]?").strip()
+            if answer == "y":
+                for ff in os.listdir(out_dir):
+                    os.remove(os.path.join(out_dir, ff))
+            elif answer == "n":
+                print("WARNING: Run cancelled")
+                sys.exit(0)
+            else:
+                print("WARNING: Please use 'y' or 'n' to answer.")
+                sys.exit(0)
+        else:
+            os.mkdir(out_dir)
+    else:
+        print("WARNING: You need to specify a project name (-p)")
+        sys.exit(0)
+
+
+    # set output paths
+    out_mapping = os.path.join(out_dir, "mapping.txt").replace("\\", "/")
+    out_report = os.path.join(out_dir, "summary.txt").replace("\\", "/")
+
+    # import fasta files
+    print("{}\t{}".format(now(), "Import target and query file"))
+    target = import_sequences(args.targets)
+    query = import_sequences(args.queries)
+    print("-" * 30)
+    print("{} target sequences".format(len(target)))
+    print("{} query sequences".format(len(query)))
+    print("-" * 30)
+
+    # do matching
+    print("{}\tSearch for motifs".format(now()))
+    matches = findall(target, query)
+    
+    # save output
+    print("{}\tSave output in project folder".format(now()))
+    save_mapping(target, matches, out_mapping)
+    save_report(target, matches, out_report)
+
+    print("{}\tRun finished".format(now()))
+    print("{} {}".format("Results successfully stored in :", out_dir))
+
+if __name__ == "__main__":
+    main()
